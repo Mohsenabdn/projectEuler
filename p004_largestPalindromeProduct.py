22ebc66290ff44cc3716aaf12a70d0a4d8e0b8a7
Mohsenabdn <mohsen.abdn@gmail.com>
2019-03-11 00:10:55 -0700

    Update p004_largestPalindromeProduct.py

--- p004_largestPalindromeProduct.py
+++ p004_largestPalindromeProduct.py
@@ -7,27 +7,29 @@ start = t.time()
 # Input : An integer number
 # Output : A bool type (True: input is palindrome, False: input is not palindrome)
 
-def isPalindrome(num):
+
+def is_palindrome(num):
     digits = []
-    while(num):
-        digits.append(num%10)
+    while num:
+        digits.append(num % 10)
         num = int(num/10)
     for i in range(int(len(digits)/2)):
         if digits[i] != digits[-(i+1)]:
             return False
     return True
 
+
 if __name__ == '__main__':
     multiples = []
-    for i in range(100, 1000):
-        for j in range(i, 1000):
-            multiples.append(i*j)
+    for k in range(100, 1000):
+        for j in range(k, 1000):
+            multiples.append(k*j)
 
     multiples.sort(reverse=True)
 
-    for i in range(len(multiples)):
-        if isPalindrome(multiples[i]):
-            print(multiples[i])
+    for j in range(len(multiples)):
+        if is_palindrome(multiples[j]):
+            print(multiples[j])
             break
 
     end = t.time()
