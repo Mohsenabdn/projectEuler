6b5ca2235a1ff90b107b9c177ad59fb857547b7f
Mohsenabdn <mohsen.abdn@gmail.com>
2019-03-10 23:44:00 -0700

    Update p002_evenFibonacciNumbers.py

--- p002_evenFibonacciNumbers.py
+++ p002_evenFibonacciNumbers.py
@@ -7,27 +7,29 @@ start = t.time()
 # Input : A bound number
 # Output : Sum of even-valued Fibonacci terms less than bound
 
-def evenValFibonacciSum(bound):
+
+def even_val_fibonacci_sum(bound):
     num1 = 3
     num2 = 5
     num3 = num1+num2
-    sum = num3
+    s = num3
 
-    while(num3 < bound):
-        sum += num3
+    while num3 < bound:
+        s += num3
         num1 = num2
         num2 = num3
         num3 = num1+num3
 
     if num2 % 2 == 0:
-        return(int(sum/2) + 2)
+        return int(s/2) + 2
     elif num1 % 2 == 0:
-        return(int((sum-num2)/2) + 2)
+        return int((s-num2)/2) + 2
     else:
-        return(int((sum-num2-num1)/2) + 2)
+        return int((s-num2-num1)/2) + 2
+
 
 if __name__ == '__main__':
-    print(evenValFibonacciSum(4000000))
+    print(even_val_fibonacci_sum(4000000))
 
     end = t.time()
     print("Run time : " + str(end-start))
