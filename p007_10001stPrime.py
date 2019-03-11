b4de1b7c98abea82705d2e448803ff2e65312e70
Mohsenabdn <mohsen.abdn@gmail.com>
2019-03-11 14:05:31 -0700

    Update p007_10001stPrime.py

--- p007_10001stPrime.py
+++ p007_10001stPrime.py
@@ -1,13 +1,13 @@
 # Finding the 10,001st prime number
 
-from p003_largestPrimeFactor import primeFinder
+from p003_largestPrimeFactor import prime_finder
 import numpy as np
 import time as t
 
 start = t.time()
 
 bound = 200000
-primes = np.where(primeFinder(bound))[0]
+primes = np.where(prime_finder(bound))[0]
 print(primes[10000])
 
 end = t.time()
