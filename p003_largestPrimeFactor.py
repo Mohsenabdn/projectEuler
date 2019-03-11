b186fc3bd88c313efdb82dddf3a6e592e37abf45
Mohsenabdn <mohsen.abdn@gmail.com>
2019-03-11 00:00:43 -0700

    Update p003_largestPrimeFactor.py

--- p003_largestPrimeFactor.py
+++ p003_largestPrimeFactor.py
@@ -8,28 +8,30 @@ start = t.time()
 # Input : A bound number
 # Output : A bool list of prime numbers below bound (True: is prime, False: is not prime)
 
-def primeFinder(bound):
-    primes = [True for i in range(bound+1)]
-    primes[0] = False
-    primes[1] = False
 
-    for i in range(2,int(np.sqrt(bound))+1):
-        if primes[i]:
-            for k in range(2,int(bound/i)+1):
-                primes[i*k] = False
-    return primes
+def prime_finder(bound):
+    prime_nums = [True]*(bound+1)
+    prime_nums[0] = False
+    prime_nums[1] = False
+
+    for j in range(2, int(np.sqrt(bound))+1):
+        if prime_nums[j]:
+            for k in range(2, int(bound/j)+1):
+                prime_nums[j*k] = False
+    return prime_nums
+
 
 if __name__ == '__main__':
     num = 600851475143
     largestFact = 1
-    primes = np.where(primeFinder(int(np.sqrt(num))+1))[0]
+    primes = np.where(prime_finder(int(np.sqrt(num))+1))[0]
 
     for i in range(len(primes)):
-        while(num%primes[i] == 0):
+        while num % primes[i] == 0:
             num = int(num/primes[i])
             largestFact = primes[i]
 
-    if num > largestFact :
+    if num > largestFact:
         largestFact = num
     print('The largest Factor : ' + str(largestFact))
 
