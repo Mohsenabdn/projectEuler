1be3a25ded2ab41927b118aeb773221eaadd8ad9
Mohsenabdn <mohsen.abdn@gmail.com>
2019-09-26 16:05:08 -0700

    Update p003_largestPrimeFactor.py

--- p003_largestPrimeFactor.py
+++ p003_largestPrimeFactor.py
@@ -1,39 +1,40 @@
 # Finding the largest prime factor of 600851475143
 
-import numpy as np
+from numpy import where
 import time as t
 
 start = t.time()
 
 
 def prime_finder(bound):
-    # Input : A bound number
-    # Output : A bool list of prime numbers below bound (True: is prime, False: is not prime)
+    """ Input : A bound number
+    Output : A bool list of prime numbers below bound (True: is prime, False: is not prime) """
 
     prime_nums = [True]*(bound+1)
     prime_nums[0] = False
     prime_nums[1] = False
 
-    for i in range(2, int(np.sqrt(bound))+1):
+    for i in range(2, int(bound ** 0.5)):
         if prime_nums[i]:
-            for k in range(2, int(bound/i)+1):
-                prime_nums[i*k] = False
+            for k in range(i*2, bound+1, i):
+                prime_nums[k] = False
     return prime_nums
 
 
 if __name__ == '__main__':
     num = 600851475143
-    largestFact = 1
-    primes = np.where(prime_finder(int(np.sqrt(num))+1))[0]
+    largestFactor = 1
+    primes = where(prime_finder(int(num ** 0.5)))[0]
 
     for p in primes:
         while num % p == 0:
             num = int(num/p)
-            largestFact = p
+            largestFactor = p
 
-    if num > largestFact:
-        largestFact = num
-    print('The largest Factor : ' + str(largestFact))
+    if num > largestFactor:
+        largestFactor = num
+    print('The largest Factor : ' + str(largestFactor))
 
     end = t.time()
     print('Run time : ' + str(end - start))
+    
