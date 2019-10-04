08f26d66805af0c4528b60f1fa9ab30c3e7c4928
Mohsenabdn <mohsen.abdn@gmail.com>
2019-10-03 17:30:19 -0700

    Update p005_smallestMultiple.py

--- p005_smallestMultiple.py
+++ p005_smallestMultiple.py
@@ -1,4 +1,5 @@
-# Finding the smallest positive number that is evenly divisible by all the numbers from 1 to 20
+# Finding the smallest positive number that is evenly divisible by all the
+# numbers from 1 to 20
 
 from p003_largestPrimeFactor import prime_finder
 import numpy as np
