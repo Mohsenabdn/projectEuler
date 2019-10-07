0c9c50b942510018568be2b41445fb109ff05e84
Mohsenabdn <mohsen.abdn@gmail.com>
2019-10-07 14:53:47 -0700

    Add files via upload

---
+++ p003_largestPrimeFactor.py
@@ -0,0 +1,31 @@
+# Finding the largest prime factor of 600851475143
+
+import time as t
+
+
+def prime_factors(num):
+    """ Input : The positive integer number.
+    Output : The list of prime factors of Input. """
+
+    p_facts = []
+    app = p_facts.append
+    p = 2
+    while p*p < num:
+        if num % p:
+            p += 1
+        else:
+            app(p)
+            num //= p
+    if num > 1:
+        app(num)
+    return p_facts
+
+
+if __name__ == '__main__':
+    start = t.time()
+
+    num = 600851475143
+    print('The largest Factor : ' + str(prime_factors(num)[-1]))
+
+    end = t.time()
+    print('Run time : ' + str(end - start))
