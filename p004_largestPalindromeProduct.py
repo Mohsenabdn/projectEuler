23469b2ecebf0e38d3796a8e27d9220253e997a9
Mohsenabdn <mohsen.abdn@gmail.com>
2019-10-11 16:29:19 -0700

    Update p004_largestPalindromeProduct.py

--- p004_largestPalindromeProduct.py
+++ p004_largestPalindromeProduct.py
@@ -3,28 +3,25 @@
 import numpy as np
 import time as t
 
-start = t.time()
-
 
 def is_palindrome(num):
     """ Input : An integer number
     Output : A bool type (True: input is palindrome, False: input is not
     palindrome) """
 
-    digits = []
-    while num:
-        digits.append(num % 10)
-        num = int(num/10)
-    for i in range(int(len(digits)/2)):
-        if digits[i] != digits[-(i+1)]:
+    numStr = str(num)
+    for i in range(len(numStr)//2):
+        if numStr[i] != numStr[-(i+1)]:
             return False
     return True
 
 
 if __name__ == '__main__':
+    start = t.time()
+    
     prods = (np.reshape(np.arange(100, 1000), (1, 900)) *
              np.reshape(np.arange(100, 1000), (900, 1)))[np.tril_indices(900)]
-    prods = np.unique(np.sort(prods))[::-1]
+    prods = np.sort(prods)[::-1]
 
     for j in multiples:
         if is_palindrome(j):
