2811e99ac8614b9d481bd76c4481d535d6eb817e
Mohsenabdn <mohsen.abdn@gmail.com>
2019-10-12 08:48:28 -0700

    Update p005_smallestMultiple.py

--- p005_smallestMultiple.py
+++ p005_smallestMultiple.py
@@ -1,23 +1,40 @@
 # Finding the smallest positive number that is evenly divisible by all the
 # numbers from 1 to 20
 
-from p003_largestPrimeFactor import prime_finder
 import numpy as np
 import time as t
 
-start = t.time()
 
-topNum = 20
-smallest = 1
-primes = np.where(prime_finder(topNum))[0]
+def prime_finder(bound):
+    """ Input : A bound number
+    Output : A bool list of prime numbers below bound (True: is prime,
+    False: is not prime)"""
 
-for p in primes:
-    i = 1
-    while p**i < topNum:
-        smallest *= p
-        i += 1
+    prime_nums = [True]*(bound+1)
+    prime_nums[0] = False
+    prime_nums[1] = False
 
-print(smallest)
+    for i in range(2, int(bound ** 0.5)):
+        if prime_nums[i]:
+            for k in range(i*2, bound+1, i):
+                prime_nums[k] = False
+    return prime_nums
 
-end = t.time()
-print('Run time : ' + str(end-start))
+
+if __name__ == '__main__':
+    start = t.time()
+
+    topNum = 20
+    smallest = 1
+    primes = np.where(prime_finder(topNum))[0]
+
+    for p in primes:
+        i = 1
+        while p**i < topNum:
+            smallest *= p
+            i += 1
+
+    print(smallest)
+
+    end = t.time()
+    print('Run time : ' + str(end-start))
