2af0a4dbf3c23d4b23e855b7c4eed5840d7834f0
Mohsenabdn <mohsen.abdn@gmail.com>
2019-10-20 15:55:06 -0700

    Update p008_largestProductOfSeries.py

--- p008_largestProductOfSeries.py
+++ p008_largestProductOfSeries.py
@@ -1,22 +1,24 @@
-# Finding 13 adjacent digits in bigNum (a 1000-digit number) having the greatest product.
+# Finding 13 adjacent digits of a big Number (a 1000-digit number saved in the
+# file named p008_1000DigitNumber.txt) having the greatest product.
 
 import numpy as np
 import time as t
 
-start = t.time()
-
 
 def product_adjacent(big_num, num_adj):
-    # Input : A large number of type string and the number of adjacent digits of type int
-    # Output : A numpy array of all products of num_adj adjacent digits
+    """ Input : A large number of type string (big_num) and the number of
+     adjacent digits (num_adj).
+     Output : A list of all products of num_adj adjacent digits. """
     
     prod_len = len(big_num)-num_adj+1
-    digits = np.array([int(i) for i in big_num])
-    prod = np.array([np.prod(digits[i:i+num_adj]) for i in range(prod_len)])
+    digits = [int(i) for i in big_num]
+    prod = [np.prod(digits[i:i+num_adj]) for i in range(prod_len)]
     return prod
 
 
 if __name__ == '__main__:
+    start = t.time()
+    
     file = open('1000DigitNumber.txt')
     bigNum = file.read()
     file.close()
