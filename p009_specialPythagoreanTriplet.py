c52d46383749048fe90dcd41ef8e208bff2b7e1f
Mohsenabdn <mohsen.abdn@gmail.com>
2019-10-22 10:28:29 -0700

    Update p009_specialPythagoreanTriplet.py

--- p009_specialPythagoreanTriplet.py
+++ p009_specialPythagoreanTriplet.py
@@ -1,21 +1,28 @@
-# Finding the product of Pythagorean triplet(num1 < num2 < num3 && num3**2 = num2**2 + num1**2)\
-# whose sum is 1000 (There is exactly one)
+# Finding the product of Pythagorean triplet(num1 < num2 < num3 && num3**2
+# = num2**2 + num1**2) whose sum is 1000 (There is exactly one)
 
 import time as t
 
-start = t.time()
 
-pythTripSum = 1000
-isPythTrip = False
+def pythgorean_triple(trip_sum):
+    """ Input : The sum of Pythagorean Triplet.
+    Output : Three integers forming Pythagorean triplet whose sum is Input. """
 
-for num1 in range(1, int(pythTripSum/3)):
-    for num2 in range(num1+1, int((pythTripSum-num1)/2)):
-        if (pythTripSum-num1-num2)**2 == num1**2 + num2**2:
-            isPythTrip = True
-            print(num1*num2*(pythTripSum-num2-num1))
-            break
-    if isPythTrip:
-        break
+    for num1 in range(1, trip_sum//3):
+        for num2 in range(num1+1, (trip_sum-num1)//2 + 1):
+            if (trip_sum-num1-num2)**2 == num1**2 + num2**2:
+                return num1, num2, (trip_sum-num1-num2)
+    raise Exception('There are no Pythagorean triplet whose sum is {}'.format
+                    (trip_sum))
 
-end = t.time()
-print('Run time : ' + str(end-start))
+
+if __name__ == '__main__':    
+    start = t.time()
+
+    pythTripSum = 1000
+    num1, num2, num3 = pythgorean_triple(pythTripSum)
+    print('The Pythagorean triplet are {}, {}, {}. and their product is {}'.
+          format(num1, num2, num3, num1*num2*num3))
+
+    end = t.time()
+    print('Run time : ' + str(end-start))
