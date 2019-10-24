9874e85b37043a091d3da25ed6ba0c042f24b752
Mohsenabdn <mohsen.abdn@gmail.com>
2019-10-24 15:53:41 -0700

    Update p011_largestProductOfGrid.py

--- p011_largestProductOfGrid.py
+++ p011_largestProductOfGrid.py
@@ -1,26 +1,27 @@
-# Finding the greatest product of four adjacent numbers in the same direction\
-# (up & down, left & right, or diagonally) in the 20 by 20 grid given in the file\
-# 20By20Grid.txt
+# Finding the greatest product of four adjacent numbers in all directions
+# (up & down, left & right, or diagonally) of the 20 by 20 grid given in the
+# file '20By20Grid.txt'.
 
 import numpy as np
 import time as t
 
 
 def all_direction_products(g, num_adj):
-    # Input : A square grid and number of adjacent numbers
-    # Output : Four arrays consisting products of adjacent numbers in all directions
+    """ Input : A square grid and number of adjacent numbers.
+    Output : Four arrays consisting products of num_adj adjacent numbers in
+     each direction. """
 
     g_size = g.shape[0]
-    row_prods = np.array([np.prod(g[i:i+num_adj, j]) for j in range(g_size)
-                          for i in range(g_size-num_adj+1)])
-    col_prods = np.array([np.prod(g[i, j:j+num_adj]) for i in range(g_size)
-                          for j in range(g_size-num_adj+1)])
-    main_diag_prods = np.array([np.prod(np.diagonal(g, offset=i)[j:j+numAdj])
-                                for i in range(-(g_size-num_adj), g_size-num_adj+1)
-                                for j in range((g_size-num_adj)-np.absolute(i)+1)])
-    oppos_diag_prods = np.array([np.prod(np.diagonal(np.rot90(g), offset=i)[j:j+numAdj])
-                                 for i in range(-(g_size-num_adj), g_size-num_adj+1)
-                                 for j in range((g_size-num_adj)-np.absolute(i)+1)])
+    row_prods = [np.prod(g[i:i+num_adj, j]) for j in range(g_size)
+                 for i in range(g_size-num_adj+1)]
+    col_prods = [np.prod(g[i, j:j+num_adj]) for i in range(g_size)
+                 for j in range(g_size-num_adj+1)]
+    main_diag_prods = [np.prod(np.diagonal(g, offset=i)[j:j+numAdj])
+                       for i in range(-(g_size-num_adj), g_size-num_adj+1)
+                       for j in range((g_size-num_adj)-np.absolute(i)+1)]
+    oppos_diag_prods = [np.prod(np.diagonal(np.rot90(g), offset=i)[j:j+numAdj])
+                        for i in range(-(g_size-num_adj), g_size-num_adj+1)
+                        for j in range((g_size-num_adj)-np.absolute(i)+1)]
     return row_prods, col_prods, main_diag_prods, oppos_diag_prods
 
 
